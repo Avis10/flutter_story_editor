a16f34cd003ec306c6a546ce9789c6ace2d2129a
Avis10 <71549033+Avis10@users.noreply.github.com>
2025-04-24 12:35:55 +1200

    Update style.dart
    Color updated to blue

--- lib/src/theme/style.dart
+++ lib/src/theme/style.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
 
-const darkGreenColor = Color.fromRGBO(31, 44, 52, 1);
-const tealColor = Color.fromRGBO(0, 167, 131, 1);
+const darkGreenColor = Color(0xFF044F8D);
+const tealColor = Color(0xff075081); 
 
